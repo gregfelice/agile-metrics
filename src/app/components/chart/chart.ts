19aa603066156c0f96f812431f618a985fa98bcc
Foo <gregf@Foos-MacBook-Pro.local>
2016-03-31 21:02:51 -0400

    initial commit

---
+++ src/app/components/chart/chart.ts
@@ -0,0 +1,22 @@
+
+import {Component} from 'angular2/core';
+import {Http} from 'angular2/http';
+
+@Component({
+  selector: 'chart',
+  templateUrl: 'app/components/chart/chart.html',
+  styleUrls: ['app/components/chart/chart.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Chart {
+
+  constructor(http: Http) {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
